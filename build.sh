7b4257ee5d93172702ae85d55eba7258a46d69d4
Coleman <gibsonjc@rose-hulman.edu>
2016-10-06 14:55:45 +0000

    Added .gitignore, removed .pyc and build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-
-# materialize
-cp node_modules/materialize-css/dist/css/materialize.min.css ./static/css/
-cp node_modules/materialize-css/dist/js/materialize.min.js ./static/js/
-cp -r node_modules/materialize-css/fonts/* ./static/fonts/
-
-# jquery
-cp node_modules/jquery/dist/jquery.min.js ./static/js/
